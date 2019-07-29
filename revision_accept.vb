96bb3949928125c8f9ab05885ac716e3328ccb62
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2019-07-29 22:07:56 +0200

    Add files via upload

--- revision_accept.vb
+++ revision_accept.vb
@@ -1,8 +1,12 @@
-Sub tracked_to_red()           
-    ActiveDocument.TrackRevisions = False    
-    For Each Change In ActiveDocument.Revisions        
-        Set myRange = Change.Range
-        myRange.Revisions.AcceptAll
-        myRange.Font.Color = wdColorRed
-    Next    
+' stops tracking, accepts all tracked changes and colors them red
+
+Sub tracked_to_red()
+    ActiveDocument.TrackRevisions = False
+    For Each storyRange In ActiveDocument.StoryRanges
+        For Each Change In storyRange.Revisions
+            Set myRange = Change.Range
+            myRange.Revisions.AcceptAll
+            myRange.Font.Color = wdColorRed
+        Next
+    Next
 End Sub
