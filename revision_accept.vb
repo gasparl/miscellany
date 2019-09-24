35c0e75275213b678745ebb47ce885f795829e72
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2019-09-24 18:36:20 +0200

    Update revision_accept.vb

--- revision_accept.vb
+++ revision_accept.vb
@@ -1,4 +1,6 @@
 ' stops tracking, accepts all tracked changes and colors them red
+' open MS Word document, press Alt-F11 to open VBA, open new module
+' copy-paste the code below and run with F5 (or select "Run" in menu)
 
 Sub tracked_to_red()
     ActiveDocument.TrackRevisions = False
