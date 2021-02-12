026d0d63ade4e1b2195f864430183ac20c0b6b29
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2021-02-12 17:47:58 +0100

    blue

--- revision_accept.vb
+++ revision_accept.vb
@@ -1,6 +1,7 @@
 ' stops tracking, accepts all tracked changes and colors them red
 ' open MS Word document, press Alt-F11 to open VBA, open new module
 ' copy-paste the code below and run with F5 (or select "Run" in menu)
+' use wdColorRed for red
 
 Sub tracked_to_red()
     ActiveDocument.TrackRevisions = False
@@ -8,7 +9,7 @@ Sub tracked_to_red()
         For Each Change In storyRange.Revisions
             Set myRange = Change.Range
             myRange.Revisions.AcceptAll
-            myRange.Font.Color = wdColorRed
+            myRange.Font.Color = wdColorBlue
         Next
     Next
 End Sub
